42643a1039107a1e6c9946be9b79a3e6aad4fff4
Arash <aras72h@gmail.com>
2024-12-09 22:11:22 +0330

    Flash square 14030919

--- index.js
+++ index.js
@@ -1,31 +1,62 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
 ctx.canvas.width = 600;
 ctx.canvas.height = 400;
 
 const rectangle = {
     width: 50,
     height: 50,
-    x: 0,
+    x: 275,
     y: 175,
 }
 
-let velocity = 2;
-const loop = function() {
-    ctx.fillStyle = '#ffc';
-    ctx.fillRect(0, 0, 600, 400)
-    ctx.fillStyle = '#f55'
-    ctx.beginPath();
-    ctx.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-    ctx.fill();
-    rectangle.x+=velocity;
+const draw = function () {
+    drawBg();
+    flash(5)
+};
 
-    if (rectangle.x + rectangle.width > 600 || rectangle.x < 0) {
-        velocity *= -1;
-    }
 
-    window.requestAnimationFrame(loop);
+const flash = function (count) {
+    if (count === 0) return;
+
+    setTimeout(function () {
+        drawRectangle();
+        setTimeout(function () {
+            drawBg();
+            flash(count - 1);
+        }, 500)
+    }, 500)
 }
 
-window.requestAnimationFrame(loop);
+
+const drawBg = function () {
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(0, 0, 600, 400);
+}
+
+
+const drawRectangle = function () {
+    ctx.beginPath();
+    ctx.fillStyle = '#000';
+    ctx.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+    ctx.fill();
+}
+
+draw();
+
+// const loop = async function() {
+//     ctx.fillStyle = '#fff';
+//     ctx.fillRect(0, 0, 600, 400);
+//     ctx.beginPath();
+//     ctx.fillStyle = `rgb(0, 0, 0)`;
+//     ctx.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+//     ctx.fill();
+//
+//     setTimeout(() => {
+//         ctx.clearRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+//     }, 50);
+//
+//     window.requestAnimationFrame(loop);
+// }
+//
+// window.requestAnimationFrame(loop);
