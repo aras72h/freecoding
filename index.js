97a6af3d6d9d0649e40993435a2afc0de44308ca
Arash <aras72h@gmail.com>
2024-12-10 18:26:49 +0330

    Draw smiley 14030920

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 ctx.canvas.width = 600;
 ctx.canvas.height = 400;
+ctx.rect(0, 0, 600, 400)
+ctx.stroke();
 
-ctx.fillStyle = 'blue';
 ctx.beginPath();
-ctx.moveTo(300, 200);
-ctx.lineTo(300,100);
-ctx.lineTo(200, 200);
-ctx.lineTo(300, 300);
-ctx.closePath();
 
-ctx.fill();
+ctx.arc(300, 200, 200, 0, Math.PI * 2, true);
+ctx.moveTo(450, 200);
+ctx.arc(300, 200, 150, 0, Math.PI, false);
+ctx.moveTo(250, 150);
+ctx.arc(225, 150, 25, 0, Math.PI * 2, true);
+ctx.moveTo(400, 150);
+ctx.arc(375, 150, 25, 0, Math.PI * 2, true);
+
+
+ctx.stroke();
