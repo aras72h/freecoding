cd61b701593b0dda0c94551db5724fe469d155c3
Arash <aras72h@gmail.com>
2024-12-10 18:05:30 +0330

    Draw triangle 14030920

--- index.js
+++ index.js
@@ -1,62 +1,14 @@
-const canvas = document.getElementById('canvas');
+const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 ctx.canvas.width = 600;
 ctx.canvas.height = 400;
 
-const rectangle = {
-    width: 50,
-    height: 50,
-    x: 275,
-    y: 175,
-}
+ctx.fillStyle = 'blue';
+ctx.beginPath();
+ctx.moveTo(300, 200);
+ctx.lineTo(300,100);
+ctx.lineTo(200, 200);
+ctx.lineTo(300, 300);
+ctx.closePath();
 
-const draw = function () {
-    drawBg();
-    flash(5)
-};
-
-
-const flash = function (count) {
-    if (count === 0) return;
-
-    setTimeout(function () {
-        drawRectangle();
-        setTimeout(function () {
-            drawBg();
-            flash(count - 1);
-        }, 500)
-    }, 500)
-}
-
-
-const drawBg = function () {
-    ctx.fillStyle = '#fff';
-    ctx.fillRect(0, 0, 600, 400);
-}
-
-
-const drawRectangle = function () {
-    ctx.beginPath();
-    ctx.fillStyle = '#000';
-    ctx.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-    ctx.fill();
-}
-
-draw();
-
-// const loop = async function() {
-//     ctx.fillStyle = '#fff';
-//     ctx.fillRect(0, 0, 600, 400);
-//     ctx.beginPath();
-//     ctx.fillStyle = `rgb(0, 0, 0)`;
-//     ctx.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-//     ctx.fill();
-//
-//     setTimeout(() => {
-//         ctx.clearRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-//     }, 50);
-//
-//     window.requestAnimationFrame(loop);
-// }
-//
-// window.requestAnimationFrame(loop);
+ctx.fill();
