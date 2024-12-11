913c85670b418eba016c6fb4a6abf87cf3654e87
Arash <aras72h@gmail.com>
2024-12-11 18:53:02 +0330

    Draw more lines 14030921

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ ctx.canvas.height = 400;
 ctx.rect(0, 0, 600, 400)
 ctx.stroke();
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < ctx.canvas.height / 10; i++) {
     ctx.beginPath();
-    ctx.moveTo(0, i * 100);
+    ctx.moveTo(0, i * 10);
     ctx.lineTo(600, i * 100);
     ctx.stroke();
 }
